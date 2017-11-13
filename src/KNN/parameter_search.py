3e97120a910d2b59fa456e3ba91be1032f1db050
fpenovi <fpenovi@certant.com>
2017-11-13 18:11:31 -0300

    Dejo links para leer en casa

--- src/KNN/parameter_search.py
+++ src/KNN/parameter_search.py
@@ -3,14 +3,19 @@
 
 # PYTHON imports hack
 #################################################################
-import sys, os                                                  #
-file_dir = os.path.dirname(os.path.realpath(__file__))          #
-sys.path.insert(0, os.path.abspath(os.path.dirname(file_dir)))  #
+#import sys, os                                                  #
+#file_dir = os.path.dirname(os.path.realpath(__file__))          #
+#sys.path.insert(0, os.path.abspath(os.path.dirname(file_dir)))  #
 #################################################################
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from utils import KFold as kf
+
+# INFO: https://www.quantstart.com/articles/Using-Cross-Validation-to-Optimise-a-Machine-Learning-Method-The-Regression-Setting
+# -> este va piola INFO: https://stackoverflow.com/questions/45115964/separate-pandas-dataframe-using-sklearns-kfold
+# http://scikit-learn.org/stable/auto_examples/model_selection/plot_grid_search_digits.html#sphx-glr-auto-examples-model-selection-plot-grid-search-digits-py
+
+# http://maciejjaskowski.github.io/2016/01/22/pandas-scikit-workflow.html -> (muy lindo, empezar por aca)
 
 
 
